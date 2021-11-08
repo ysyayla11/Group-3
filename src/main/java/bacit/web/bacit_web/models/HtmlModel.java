8ab2a719ef923dec1ecb93c44994d00ad4020921
Yavuz Selim Yayla <80274450+ysyayla11@users.noreply.github.com>
2021-11-08 11:30:21 +0100

    Update HtmlModel.java

--- src/main/java/bacit/web/bacit_web/models/HtmlModel.java
+++ src/main/java/bacit/web/bacit_web/models/HtmlModel.java
@@ -14,7 +14,7 @@ public class HtmlModel {
             "\n" +
             "<nav>\n" +
             "    <a class=\"active\" href=\"../SiteUser/home.jsp\"><button type =\"button\" class=\"nav-button\">Hjem</button></a>\n" +
-            "    <a href=\"../SiteUser/DinProfil.jsp\"><button type =\"button\" class=\"nav-button\">Din Profil</button></a>\n" +
+            "    <a href=\"../SiteUser/GetUserProfileServlet\"><button type =\"button\" class=\"nav-button\">Din Profil</button></a>\n" +
             "    <a href=\"../SiteUser/GetBookingServlet\" methods=\"get\"><button type =\"button\" class=\"nav-button\">Dine Bestillinger</button></a>\n" +
             "</nav>";
 
